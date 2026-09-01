44aaca0ef51c8e5ae2ef4328d582ed75bbc73f8b
Michael Pariaszevski <41803503+MichaelPariaszevski@users.noreply.github.com>
2026-08-31 23:33:14 -0700

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left, right = 1, max(piles)
+        middle = (left + right) // 2
+        result = middle
+        while left <= right: 
+            middle = (left + right) // 2
+            if self.rate_is_valid(middle, piles, h): 
+                right = middle - 1
+                result = middle
+            else: 
+                left = middle + 1
+
+        return result
+        
+    def rate_is_valid(self, rate: int, piles: List[int], h: int) -> bool: 
+            total_hours = 0
+            for i in range(len(piles)): 
+                curr_pile = piles[i]
+                hours_needed_for_pile = math.ceil(curr_pile / rate)
+                total_hours += hours_needed_for_pile
+
+            if total_hours > h: 
+                return False
+            else: 
+                return True
+
+            
